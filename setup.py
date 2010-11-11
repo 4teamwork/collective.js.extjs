51bc72e7eb6c5fe97b5eb3c9d984003ec9a3072d
Philippe Gross <p.gross@4teamwork.ch>
2010-11-11 13:48:16 +0100

    upated setup.py (4teamwork standards)

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ from setuptools import setup, find_packages
 import os
 
 version = open('collective/js/extjs/version.txt').read().strip()
+maintainer = 'Victor baumann'
 
 setup(name='collective.js.extjs',
       version=version,
-      description="Ext JS Cross-Browser Rich Internet Application Framework",
+      description="Ext JS Cross-Browser Rich Internet Application Framework \
+        (Maintainer: %s)" % maintainer,
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
@@ -13,8 +15,9 @@ setup(name='collective.js.extjs',
         "Programming Language :: Python",
         ],
       keywords='',
-      author='Victor BAUMANN',
-      author_email='v.baumann@4teamwork.ch',
+      author='%s, 4teamwork GmbH'  % maintainer,
+      author_email='mailto:info@4teamwork.ch',
+      maintainer=maintainer,
       url='http://www.sencha.com/products/js/',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
