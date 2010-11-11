f61f05d91357dfd7333975d609abe4c829383e73
Philippe Gross <p.gross@4teamwork.ch>
2010-11-11 11:31:22 +0100

    setup.py: using version.txt

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0'
+version = open('collective/js/extjs/version.txt').read().strip()
 
 setup(name='collective.js.extjs',
       version=version,
